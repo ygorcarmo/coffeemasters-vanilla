2b42c4cf50c60563b21d857a58f1f6377d074001
Ygor Carmo <ygor.carmo@gmail.com>
2023-07-24 16:45:02 +1000

    stopped on changing dom element

---
+++ services/Router.js
@@ -0,0 +1,23 @@
+const Router = {
+  init: () => {
+    document.querySelectorAll("a.navlink").forEach((a) => {
+      a.addEventListener("click", (event) => {
+        event.preventDefault();
+        // const url1 = event.target.href
+        const url = event.target.getAttribute("href");
+        Router.go(url);
+      });
+    });
+    // Check the initial URL
+    Router.go(location.pathname);
+  },
+  go: (route, addToHistory = true) => {
+    console.log(`Going to ${route}`);
+
+    if (addToHistory) {
+      history.pushState({ route }, "", route);
+    }
+  },
+};
+
+export default Router;
